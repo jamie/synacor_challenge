cf94a6acfc91040d707fe71d004c2170ad9a402c
Jamie Macey <jamie.git@tracefunc.com>
2023-03-15 22:12:08 -0700

    Refactor to implicit instruction pointer

--- synacor.rb
+++ synacor.rb
@@ -21,27 +21,32 @@ class VM
     @input = []
   end
 
-  def op = prog[ip]
+  def incr = @ip += 1
 
-  def a = value(1)
+  def op
+    prog[ip].tap { incr }
+  end
 
-  def b = value(2)
+  def a = value
 
-  def c = value(3)
+  def b = value
+
+  def c = value
 
   def push(data) = @stack.push(data)
 
   def pop = @stack.pop
 
-  def value(i)
-    num = prog[ip + i]
+  def value
+    num = prog[ip].tap { incr }
     return num if num <= 32767
     return @reg[num - 32768] if num < 32776
     raise "Invalid number #{num}"
   end
 
-  def write_a(val)
-    num = prog[ip + 1]
+  def write_a
+    num = prog[ip].tap { incr }
+    val = yield
     raise "expected register reference, got literal `a` #{num}" if num < 32768
     @reg[num - 32768] = val % 32768
   end
@@ -52,84 +57,61 @@ class VM
       when 0 # halt
         break
       when 1 # set a b
-        write_a(b)
-        @ip += 3
+        write_a { b }
       when 2 # push a
         push(a)
-        @ip += 2
       when 3 # pop a
-        write_a(pop)
-        @ip += 2
+        write_a { pop }
       when 4 # eq a b c
-        if b == c
-          write_a(1)
-        else
-          write_a(0)
-        end
-        @ip += 4
+        write_a { (b == c) ? 1 : 0 }
       when 5 # gt a b c
-        if b > c
-          write_a(1)
-        else
-          write_a(0)
-        end
-        @ip += 4
+        write_a { (b > c) ? 1 : 0 }
       when 6 # jmp a
         @ip = a
       when 7 # jt a b
         if a > 0
           @ip = b
         else
-          @ip += 3
+          b
         end
       when 8 # jf a b
         if a == 0
           @ip = b
         else
-          @ip += 3
+          b
         end
       when 9 # add a b c
-        write_a(b + c)
-        @ip += 4
+        write_a { b + c }
       when 10 # mult a b c
-        write_a(b * c)
-        @ip += 4
+        write_a { b * c }
       when 11 # mod a b c
-        write_a(b % c)
-        @ip += 4
+        write_a { b % c }
       when 12 # and a b c
-        write_a(b & c)
-        @ip += 4
+        write_a { b & c }
       when 13 # or a b c
-        write_a(b | c)
-        @ip += 4
+        write_a { b | c }
       when 14 # not a b
-        write_a(~b)
-        @ip += 3
+        write_a { ~b }
       when 15 # rmem a b
-        write_a(prog[b])
-        @ip += 3
+        write_a { prog[b] }
       when 16 # wmem a b
         prog[a] = b
-        @ip += 3
       when 17 # call a
-        push(ip + 2)
+        push(ip + 1) # after consuming op
         @ip = a
       when 18 # ret
-        break if @stack.empty?
+        raise "RET: empty stack" if @stack.empty?
         @ip = pop
       when 19 # out a
         print a.chr
-        @ip += 2
       when 20 # in a
         @input = gets.bytes if @input.empty?
-        write_a(@input.shift)
-        @ip += 2
+        write_a { @input.shift }
       when 21 # noop
-        @ip += 1
+        42
       else
         puts
-        puts "!! Unknown opcode: #{op}"
+        puts "!! Unknown opcode: #{prog[ip]}"
         break
       end
     end
